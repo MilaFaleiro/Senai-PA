18d99e97e5dbbcb1cd680daae0b1cbea0d921faf
Mila Faleiro <camilagf83@gmail.com>
2025-10-13 16:52:32 -0300

    Add files via upload

---
+++ frontend/src/assets/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+        <p>© 2024 My Company. All rights reserved.</p>
+        </footer>
+  );
+}
+export default Footer;
---
+++ frontend/src/assets/components/pages/HomePage/index.jsx
@@ -0,0 +1,15 @@
+// frontend\src\assets\components\pages\HomePage\index.jsx
+
+import './styles.css';
+
+function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Bem-vindo ao Sistema de Clientes</h1>
+      <p>Gerencie seus clientes de forma eficiente e prática.</p>
+        <p>Ultilize o menu para navegar entre as opções disponíveis.</p>
+    </div>
+  );
+}
+
+export default HomePage;
